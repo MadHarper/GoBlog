7087f3d23723907f4b48247b3b1df6900de45bed
Alex Kolesnikov <alex_maykop@yahoo.com>
2017-06-29 14:04:15 +0300

    Добавлен файловый сервер

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 )
 
 func main(){
+	fmt.Println("Listening on port :3000")
 	http.HandleFunc("/", indexHandler)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+
 
 	http.ListenAndServe(":3000", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
-	t, err := template.ParseFiles("templates/index.html")
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil{
 		fmt.Fprint(w, err.Error())
 	}
-
 	t.ExecuteTemplate(w, "index", nil)
 }
