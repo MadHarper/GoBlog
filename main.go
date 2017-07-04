e1ea50bd15b1d6d4b651106b167cc31346dfcae7
Alex Kolesnikov <alex_maykop@yahoo.com>
2017-07-04 07:26:29 +0300

    Частично работающая форма добавления заметка. Пока без сохранения

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"net/http"
 	"fmt"
 	"html/template"
+	"github.com/MadHarper/goBlog/models"
 )
 
+var Posts map[int]*models.Post
+var n int
+
 func main(){
+	n = 1
+	Posts = make(map[int]*models.Post, 0)
+	fmt.Println(Posts)
+
 	fmt.Println("Listening on port :3000")
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/write", writeHandler)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 
 
@@ -21,4 +30,24 @@ func indexHandler(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, err.Error())
 	}
 	t.ExecuteTemplate(w, "index", nil)
+}
+
+func writeHandler(w http.ResponseWriter, r *http.Request){
+
+	if r.Method == "POST"{
+		title := r.FormValue("title")
+		content := r.FormValue("content")
+		new_post := models.NewPost(n, title, content)
+		n++
+		Posts[new_post.Id] = new_post
+		for _, value := range Posts {
+			fmt.Println(value)
+		}
+	}
+
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil{
+		fmt.Fprint(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "write", nil)
 }
---
+++ models/post.go
@@ -0,0 +1,11 @@
+package models
+
+type Post struct {
+	Id int
+	Title string
+	Content string
+}
+
+func NewPost(id int, title string, content string) *Post{
+	return &Post{id, title, content}
+}
